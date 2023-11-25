50fd35683d26f3ce2e92112bbd30bb49358c5b2f
Rohan Singh <rs7289579@gmail.com>
2023-11-25 14:04:32 +0530

    encrypt user password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,10 +1,19 @@
 const UserModel = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const CryptoJS = require("crypto-js");
 
 const registerUser = async (req, res) => {
   try {
     let { name, email, password } = req.body;
-    let user = await UserModel.create({ name, email, password });
+    var encPassword = CryptoJS.AES.encrypt(
+      password,
+      process.env.CRYPTO_SECRET
+    ).toString();
+    const user = await UserModel.create({
+      name,
+      email,
+      password: encPassword,
+    });
     res.status(201).json({ message: "User registered successfully", user });
   } catch (e) {
     res.status(500).json({ message: e.message });
@@ -16,7 +25,11 @@ const loginUser = async (req, res) => {
     let { email, password } = req.body;
     let user = await UserModel.findOne({ email });
     if (!user) return res.status(404).json({ message: "User doesn't exists" });
-    if (user.password != password) {
+    const decPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.CRYPTO_SECRET
+    ).toString(CryptoJS.enc.Utf8);
+    if (decPassword != password) {
       return res.status(403).json({ message: "Wrong credentials!" });
     }
     let token = jwt.sign(
@@ -24,7 +37,9 @@ const loginUser = async (req, res) => {
       process.env.JWT_SECRET,
       { expiresIn: "3d" }
     );
-    res.status(200).json({ message: "Login successful", token: `Bearer ${token}` });
+    res
+      .status(200)
+      .json({ message: "Login successful", token: `Bearer ${token}` });
   } catch (e) {
     res.status(500).json({ message: e.message });
   }
